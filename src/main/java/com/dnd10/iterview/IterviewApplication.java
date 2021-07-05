3aec194898ef701153f89db167e593b980561f88
RulLu16 <may05200@naver.com>
2021-07-05 21:22:21 +0900

    [ADD] first commit

---
+++ src/main/java/com/dnd10/iterview/IterviewApplication.java
@@ -0,0 +1,13 @@
+package com.dnd10.iterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd10/iterview/IterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd10.iterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
