9607c3049c02bc8bddbfb0a596518196d4655d97
최상현 <yuio1126@gmail.com>
2021-08-03 17:14:19 +0900

    fix: cookie path 관련 TODO 추가

--- src/main/java/com/dnd10/iterview/config/oauth2/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/dnd10/iterview/config/oauth2/CustomOAuth2SuccessHandler.java
@@ -4,24 +4,25 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.dnd10.iterview.config.jwt.JwtProperties;
 import com.dnd10.iterview.util.CookieUtils;
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Date;
-
 @RequiredArgsConstructor
 @Component
 public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    // TODO:: REDIRECT_URL/PATH를 나중에는 local, real 나눠서 구분해야함.
     public static final String REDIRECT_URL = "http://localhost:8080/";
+    public static final String PATH = "/";
     private final CookieUtils cookieUtils;
 
     @Override
@@ -43,7 +44,7 @@ public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHa
         response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
 
         final Cookie cookie = cookieUtils
-            .generateCookie(JwtProperties.HEADER_STRING, jwtToken, "/", false,
+            .generateCookie(JwtProperties.HEADER_STRING, jwtToken, PATH, false,
                 JwtProperties.EXPIRATION_TIME, null);
         response.addCookie(cookie);
         getRedirectStrategy().sendRedirect(request, response, REDIRECT_URL);
