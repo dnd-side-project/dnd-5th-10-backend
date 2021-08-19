99f4f8e14cfaa9c3521683b1914f3e68504614d4
최상현 <yuio1126@gmail.com>
2021-08-20 01:30:22 +0900

    fix: HitAnswerResponseDto 에 태그리스트도 추가

--- src/main/java/com/dnd10/iterview/dto/HitAnswerResponseDto.java
+++ src/main/java/com/dnd10/iterview/dto/HitAnswerResponseDto.java
@@ -1,12 +1,17 @@
 package com.dnd10.iterview.dto;
 
 import com.dnd10.iterview.entity.Answer;
+import com.dnd10.iterview.entity.QuestionTag;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 
 @Builder
 @Getter
+@AllArgsConstructor
 public class HitAnswerResponseDto extends BaseTimeEntityDto {
 
   private long id;
@@ -16,18 +21,7 @@ public class HitAnswerResponseDto extends BaseTimeEntityDto {
   private long questionId;
   private String userName;
   private String questionContent;
-
-  public HitAnswerResponseDto(long id,
-      @Length(min = 20, max = 1000, message = "content length should be 20 ~ 1000") String content,
-      long liked, long questionId, String userName, String questionContent) {
-    super();
-    this.id = id;
-    this.content = content;
-    this.liked = liked;
-    this.questionId = questionId;
-    this.userName = userName;
-    this.questionContent = questionContent;
-  }
+  private List<QuestionTagResponseDto> tags;
 
   public HitAnswerResponseDto(Answer answer) {
     super(answer.getCreatedDate(), answer.getModifiedDate());
@@ -37,6 +31,11 @@ public class HitAnswerResponseDto extends BaseTimeEntityDto {
     this.questionId = answer.getQuestion().getId();
     this.userName = answer.getUser().getUsername();
     this.questionContent = answer.getQuestion().getContent();
+
+    final List<QuestionTag> questionTagList = answer.getQuestion().getQuestionTagList();
+    this.tags = questionTagList.stream()
+        .map(e -> new QuestionTagResponseDto(e.getTagManager().getName())).collect(
+            Collectors.toList());
   }
 
 }
