c815927fc80ccbf0d147015d2961d1a8105c361c
최상현 <yuio1126@gmail.com>
2021-08-20 23:46:01 +0900

    feat: sendRedirect "?error=login" 추가하여 로그인 에러 전달함

--- src/main/java/com/dnd10/iterview/config/oauth2/CustomOAuth2FailureHandler.java
+++ src/main/java/com/dnd10/iterview/config/oauth2/CustomOAuth2FailureHandler.java
@@ -1,10 +1,6 @@
 package com.dnd10.iterview.config.oauth2;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,23 +13,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomOAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
+  private final OAuthProperties oAuthProperties;
+  public static final String ERROR_QUERY_PARAMETER = "?error=login";
+
   @Override
   public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
       throws IOException, ServletException {
 
-    final ObjectMapper objectMapper = new ObjectMapper();
-    Map<String, Object> data = new HashMap<>();
-
-    data.put(
-        "timestamp",
-        Calendar.getInstance().getTime());
-    data.put(
-        "exception",
-        exception.getMessage());
-
-    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-    response.setContentType("application/json;charset=UTF-8");
-    response.getWriter().write(objectMapper.writeValueAsString(data));
-    response.getWriter().flush();
+    getRedirectStrategy().sendRedirect(request, response, oAuthProperties.getRedirectUrl() + ERROR_QUERY_PARAMETER);
   }
 }
